97f9c14502944ba7d22c19deb6231e86a9422997
Яшина Анастасия <ayashina@tsconsulting.com>
2020-02-27 08:55:46 +0300

    opens

--- mymodule/src/module-info.java
+++ mymodule/src/module-info.java
@@ -2,5 +2,6 @@ module mymodule {
 
     exports com.tsconsulting.modules.hello;
     provides com.tsconsulting.modules.hello.Welcomer with com.tsconsulting.modules.hello.impl.WelcomerImpl;
+    opens com.tsconsulting.modules.hello;
 
 }
